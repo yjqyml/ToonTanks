2b381004dcfb0be94d885c37be4b69d39c72e6b2
yjqyml <1027588896@qq.com>
2021-02-28 00:58:18 +0800

    Initial commit

---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+	
+};
